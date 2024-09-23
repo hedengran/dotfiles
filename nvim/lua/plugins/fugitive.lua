48249f8fd2ef203affc43b263e6ebc2b1c212ee1
Gustav Hedengran <gustav.hedengran@gmail.com>
2024-09-23 14:17:28 +0200

    sync

--- nvim/lua/plugins/fugitive.lua
+++ nvim/lua/plugins/fugitive.lua
@@ -1,19 +1,29 @@
 return {
-	"tpope/vim-fugitive",
-	keys = {
-		{ "<leader>gg", ":Git<CR>", mode = { "n" }, desc = "Open git" },
-		{ "<leader>gb", ":Git blame<CR>", mode = { "n" }, desc = "Git blame" },
-		{ "<Leader>gD", ":Gvdiffsplit<CR>", mode = { "n" }, desc = "Open git diff in split view" },
-		{
-			"<Leader>go",
-			":Gvsplit origin/main:%<CR>",
-			mode = { "n" },
-			desc = "Open unedited file from origin/main in split",
+	{
+		"tpope/vim-fugitive",
+		lazy = false,
+		keys = {
+			{ "<leader>gg", ":Git<CR>", mode = { "n" }, desc = "Open git" },
+			{ "<leader>gb", ":Git blame<CR>", mode = { "n" }, desc = "Git blame" },
+			{ "<Leader>gD", ":Gvdiffsplit<CR>", mode = { "n" }, desc = "Open git diff in split view" },
+			{
+				"<Leader>go",
+				":Gvsplit origin/main:%<CR>",
+				mode = { "n" },
+				desc = "Open unedited file from origin/main in split",
+			},
 		},
+		config = function()
+			-- don't hide git buffers when switching buffers
+			-- https://github.com/tpope/vim-fugitive/discussions/1880
+			vim.cmd([[autocmd User FugitiveObject setlocal bufhidden=]])
+		end,
 	},
-	config = function()
-		-- don't hide git buffers when switching buffers
-		-- https://github.com/tpope/vim-fugitive/discussions/1880
-		vim.cmd([[autocmd User FugitiveObject setlocal bufhidden=]])
-	end,
+	{
+		"shumphrey/fugitive-gitlab.vim",
+		lazy = false,
+		config = function()
+			vim.g["fugitive_gitlab_domains"] = { "https://gitlab.evroc.dev/" }
+		end,
+	},
 }
